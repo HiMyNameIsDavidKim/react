51e612482633d80431af42eb9d2305c8874650f2
HiMyNameIsDavidKim <rkfka1401@gmail.com>
2022-12-07 14:43:24 +0900

    12/7 study update

--- uat/components/Login.jsx
+++ uat/components/Login.jsx
@@ -3,7 +3,7 @@ import { useState } from "react"
 import { loginApi } from '../api'
 const Login = () => {
     const [inputs, setInputs] = useState({})
-    const {username, password} = inputs;
+    const {email, password} = inputs;
 
     const onChange = e => {
         e.preventDefault()
@@ -20,7 +20,7 @@ const Login = () => {
 
     return (
     <>
-        USERNAME: <input type="text" name="username" onChange={onChange} /><br/>
+        EMAIL: <input type="text" name="email" onChange={onChange} /><br/>
         PASSWORD: <input type="text" name="password" onChange={onChange} /><br/>
         <button onClick={onClick}> 로그인 </button>
 
